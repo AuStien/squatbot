080288547d4ca97c0ea6579fe98862e6c79c6658
Audun Stien <auduns@stacc.com>
2022-10-31 23:34:18 +0000

    feat: add all functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
-module github.com/stacc/squatbot
+module github.com/austien/squatbot
 
 go 1.18
+
+require (
+	github.com/sirupsen/logrus v1.9.0
+	github.com/slack-go/slack v0.11.3
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -3,24 +3,113 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/austien/squatbot/squat"
+	log "github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
-
-	wait := rand.Intn(int(time.Hour) * 8)
-
-	time.Sleep(time.Duration(wait))
-
-	squats, err := Squats(time.Now().Day())
-	if err != nil {
-		panic(err)
+	apiKey, ok := os.LookupEnv("SQUATBOT_API_KEY")
+	if !ok {
+		log.Fatal("missing API key")
 	}
 
-	if squats == 0 {
-		fmt.Printf(restMessages[rand.Intn(len(restMessages))])
+	publishID, ok := os.LookupEnv("SQUATBOT_PUBLISH_ID")
+	if !ok {
+		log.Fatal("missing ID to post message")
+	}
+
+	var maxWaitDuration time.Duration
+	maxWaitDurationStr, ok := os.LookupEnv("SQUATBOT_MAX_WAIT_DURATION")
+	if !ok {
+		maxWaitDuration = time.Hour * 8 * 1000
 	} else {
-		fmt.Printf(squatMessages[rand.Intn(len(squatMessages))], squats)
+		duration, err := time.ParseDuration(maxWaitDurationStr)
+		if err != nil {
+			log.WithError(err).Fatalf("failed to parse %q as time.Duration. Make sure duration has specified unit. Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\".", maxWaitDurationStr)
+		}
+		maxWaitDuration = duration
+	}
+
+	reportID := os.Getenv("SQUATBOT_REPORT_ID")
+
+	go func() {
+		r := http.NewServeMux()
+		r.HandleFunc("/healtz", func(w http.ResponseWriter, r *http.Request) { return })
+		r.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) { return })
+
+		log.Fatal(http.ListenAndServe(":8080", r))
+	}()
+
+	api := slack.New(apiKey)
+
+	squatMessages := make([]string, len(squat.SquatMessages))
+	copy(squatMessages, squat.SquatMessages)
+	restMessages := make([]string, len(squat.RestMessages))
+	copy(restMessages, squat.RestMessages)
+
+	rand.Seed(time.Now().Unix())
+	for {
+		wait := rand.Intn(int(maxWaitDuration))
+		time.Sleep(time.Duration(wait))
+
+		squats, err := squat.Squats(time.Now().Day())
+		if err != nil {
+			if reportID != "" {
+				log.WithError(err).Error("failed to get amount of squats")
+				channel, _, postErr := api.PostMessage(reportID, slack.MsgOptionText(err.Error(), false))
+				if postErr != nil {
+					log.WithError(postErr).Errorf("failed to send error message to %q", channel)
+				}
+			} else {
+				log.WithError(err).Fatal("failed to get amount of squats")
+			}
+			continue
+		}
+
+		var msg string
+		if squats == 0 {
+			i := rand.Intn(len(restMessages))
+			msg = restMessages[i]
+			restMessages = append(restMessages[:i], restMessages[i+1:]...)
+		} else {
+			i := rand.Intn(len(squatMessages))
+			msg = fmt.Sprintf(squatMessages[i], squats)
+			squatMessages = append(squatMessages[:i], squatMessages[i+1:]...)
+		}
+
+		channel, _, err := api.PostMessage(publishID, slack.MsgOptionText(msg, false))
+		if err != nil {
+			if reportID != "" {
+				log.WithError(err).Errorf("failed to send message: %q", msg)
+				channel, _, postErr := api.PostMessage(reportID, slack.MsgOptionText(err.Error(), false))
+				if postErr != nil {
+					log.WithError(postErr).Errorf("failed to send error message to %q", channel)
+				}
+			} else {
+				log.WithError(err).Fatalf("failed to send message: %q", msg)
+			}
+			continue
+		}
+
+		log.WithField("squatMessages", squatMessages).Infof("(%s): %s", channel, msg)
+
+		if len(squatMessages) == 0 {
+			tmp := make([]string, len(squat.SquatMessages))
+			copy(tmp, squat.SquatMessages)
+			squatMessages = tmp
+			log.Info("Squat messages have been reset")
+		}
+
+		if len(restMessages) == 0 {
+			tmp := make([]string, len(squat.RestMessages))
+			copy(tmp, squat.RestMessages)
+			restMessages = tmp
+			log.Info("Rest messages have been reset")
+		}
 	}
 }
